3d2a6f1721cb87d83c80c3fdbcf4d15a7ee7175d
Craig Sharkie <craig.sharkie@gmail.com>
2013-08-31 06:21:08 +1000

    PLAY Set played letter maximum
    Set played letter maximum and made played letter apparent

--- scripts/events.js
+++ scripts/events.js
@@ -14,8 +14,18 @@ var EVENTS = {
 			BUILD.grapheme();
 		});
 
-		$('#grid').on('click', 'span', function(){
-			BSLN.play($(this).attr("class"));
+		$('#grid').on('click', 'span.racked', function(){
+			var $this = $(this);
+			var index = $('#grid').find('span').index($this);
+		
+			if (BSLN.PLAYED.length < BSLN.SLOTS) {
+				BSLN.play($this.attr("class"), index);
+
+				$this.closest("li").css("opacity", .1);
+			}
+			else {
+				alert("You can only play " + BSLN.SLOTS + " letters");
+			}
 		});
 
 		$('#solution').on('click', 'span', function(){
@@ -23,7 +33,7 @@ var EVENTS = {
 		});
 	},
 	fetch: function() {
-		BSLN.WORD = nines[this.random(nines.length - 1)].toLowerCase();
+		BSLN.NINE = nines[this.random(nines.length - 1)].toLowerCase();
 	},
 	random: function(which) {
 		return Math.floor(Math.random() * (which + 1));
